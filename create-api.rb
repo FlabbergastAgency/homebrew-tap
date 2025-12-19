dda19641ff98d91ca26f46e718d53c2a9171dfb5
Marko Opačić <marko.opacic+github@flabbergast.agency>
2025-12-19 17:33:24 +0100

    add formula for CreateAPI

---
+++ create-api.rb
@@ -0,0 +1,18 @@
+class CreateAPI < Formula
+  desc ""
+  homepage "https://github.com/FlabbergastAgency/CreateAPI"
+  url "https://github.com/FlabbergastAgency/CreateAPI/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  license "MIT"
+
+  depends_on :xcode => ["14.0", :build]
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/create-api"
+  end
+
+  test do
+    assert_match "0.2.1", shell_output("#{bin}/create-api --version")
+  end
+end
