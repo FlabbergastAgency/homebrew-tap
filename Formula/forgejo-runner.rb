9023899d4f675c0907c01edb0e67edd09ad238d2
Mario Radonic <mario.radonic90@gmail.com>
2026-01-28 18:21:41 +0100

    Added forgejo-runner.

---
+++ Formula/forgejo-runner.rb
@@ -0,0 +1,35 @@
+class ForgejoRunner < Formula
+  desc "Self-hosted runner for Forgejo Actions (macOS native)"
+  homepage "https://code.forgejo.org/forgejo/runner"
+  url "https://code.forgejo.org/forgejo/runner/archive/v12.6.2.tar.gz" # Update version as needed
+  sha256 "1dc29651a447678ddb992b7a00d6359f56a31ace3d7f1893f5419b838291bbd9" # Use 'curl -L [url] | shasum -a 256'
+  license "GPL-3.0-or-later"
+
+  depends_on "go" => :build
+
+  def install
+    # Compile the binary for Apple Silicon
+    system "go", "build", "-o", bin/"forgejo-runner", "."
+
+    # Create a default config directory
+    (etc/"forgejo-runner").mkpath
+  end
+
+  service do
+    run [opt_bin/"forgejo-runner", "daemon", "--config", etc/"forgejo-runner/config.yaml"]
+    keep_alive true
+    log_path var/"log/forgejo-runner.log"
+    error_log_path var/"log/forgejo-runner.log"
+    working_dir var/"forgejo-runner"
+  end
+
+  def caveats
+    <<~EOS
+      Before starting the service, you must register the runner:
+        forgejo-runner register --instance [URL] --token [TOKEN]
+
+      Then, generate the config file:
+        forgejo-runner generate-config > #{etc}/forgejo-runner/config.yaml
+    EOS
+  end
+end
